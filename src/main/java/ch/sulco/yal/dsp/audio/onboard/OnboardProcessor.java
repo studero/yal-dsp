f31abd8ebcd60971b23d89e3b7e14ecce2c8c408
Roman Studer <mail@romanstuder.ch>
2015-10-01 22:54:40 +0200

    update onboard processor for testing

--- src/main/java/ch/sulco/yal/dsp/audio/onboard/OnboardProcessor.java
+++ src/main/java/ch/sulco/yal/dsp/audio/onboard/OnboardProcessor.java
@@ -1,12 +1,17 @@
 package ch.sulco.yal.dsp.audio.onboard;
 
 import java.util.Set;
+import java.util.logging.Logger;
 
 import ch.sulco.yal.dsp.audio.Processor;
 import ch.sulco.yal.dsp.audio.RecordingState;
 import ch.sulco.yal.dsp.dm.Sample;
 
+import com.google.common.collect.Sets;
+
 public class OnboardProcessor implements Processor {
+
+	private final static Logger log = Logger.getLogger(OnboardProcessor.class.getName());
 
 	private final Player player;
 	private final Recorder recorder;
@@ -17,13 +22,13 @@ public class OnboardProcessor implements Processor {
 		this.recorder = recorder;
 		this.loopStore = loopStore;
 	}
-	
-	public LoopStore getLoopStore(){
-		return loopStore;
+
+	public LoopStore getLoopStore() {
+		return this.loopStore;
 	}
-	
-	public Player getPlayer(){
-		return player;
+
+	public Player getPlayer() {
+		return this.player;
 	}
 
 	@Override
@@ -33,8 +38,7 @@ public class OnboardProcessor implements Processor {
 
 	@Override
 	public Set<Integer> getChannelIds() {
-		// TODO Auto-generated method stub
-		return null;
+		return Sets.newHashSet(0);
 	}
 
 	@Override
@@ -45,8 +49,9 @@ public class OnboardProcessor implements Processor {
 
 	@Override
 	public void play() {
-		// TODO Auto-generated method stub
-
+		log.info("Play");
+		this.recorder.startRecord();
+		this.recorder.loopStarted();
 	}
 
 	@Override
@@ -57,27 +62,30 @@ public class OnboardProcessor implements Processor {
 
 	@Override
 	public void loop() {
-		// TODO Auto-generated method stub
+		log.info("Loop");
+		this.recorder.stopRecord();
 
+		log.info("Start Sample");
+		this.player.startSample(this.loopStore.getSample(0));
 	}
 
 	@Override
 	public void setChannelRecording(int channelId, boolean recording) {
-		if(recording){
-			recorder.startRecord();
-		}else{
-			recorder.stopRecord();
+		if (recording) {
+			this.recorder.startRecord();
+		} else {
+			this.recorder.stopRecord();
 		}
 	}
 
 	@Override
 	public void setSampleMute(int sampleId, boolean mute) {
-		Sample sample = loopStore.getSample(sampleId);
-		if(sample != null){
-			if(mute){
-				player.startSample(sample);
-			}else{
-				player.startSample(sample);
+		Sample sample = this.loopStore.getSample(sampleId);
+		if (sample != null) {
+			if (mute) {
+				this.player.startSample(sample);
+			} else {
+				this.player.startSample(sample);
 			}
 		}
 
