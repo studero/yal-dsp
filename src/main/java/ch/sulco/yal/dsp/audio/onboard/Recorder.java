1b09ba4b5ca10b8590a83f30a3ed41c7b4794d46
Roman Studer <mail@romanstuder.ch>
2015-10-01 22:46:58 +0200

    change recorder to use byte stream instead of byte buffer
    with byte buffer there was always a gap between two loops

--- src/main/java/ch/sulco/yal/dsp/audio/onboard/Recorder.java
+++ src/main/java/ch/sulco/yal/dsp/audio/onboard/Recorder.java
@@ -1,5 +1,6 @@
 package ch.sulco.yal.dsp.audio.onboard;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -9,12 +10,10 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
-import com.sun.corba.se.impl.ior.ByteBuffer;
-
 import ch.sulco.yal.dsp.AppConfig;
 import ch.sulco.yal.dsp.audio.RecordingState;
 
-public class Recorder implements LoopListener{
+public class Recorder implements LoopListener {
 	private final static Logger log = Logger.getLogger(Recorder.class.getName());
 
 	private final AppConfig appConfig;
@@ -22,54 +21,65 @@ public class Recorder implements LoopListener{
 	private LoopStore loopStore;
 	private RecordingState recordingState = RecordingState.STOPPED;
 	private byte[] recordedSample;
-	private ByteBuffer recordingSample;
+	private ByteArrayOutputStream recordingSample;
 
 	public Recorder(AppConfig appConfig, Player player, LoopStore loopStore) {
 		this.appConfig = appConfig;
 		this.player = player;
 		this.loopStore = loopStore;
 	}
-	
-	public RecordingState getRecordingState(){
-		return recordingState;
+
+	public RecordingState getRecordingState() {
+		return this.recordingState;
 	}
-	
-	public void startRecord(){
-		if(recordingState == RecordingState.STOPPED){
-			recordingState = RecordingState.WAITING;
-			player.addLoopListerner(this);
+
+	public void startRecord() {
+		if (this.recordingState == RecordingState.STOPPED) {
+			this.recordingState = RecordingState.WAITING;
+			this.player.addLoopListerner(this);
 		}
 	}
-	
-	public void stopRecord(){
-		recordingState = RecordingState.STOPPED;
-		player.removeLoopListerner(this);
-		if(recordedSample != null){
-			loopStore.addSample(recordedSample);
-			recordedSample = null;
-			recordingSample = null;
+
+	public void stopRecord() {
+		this.recordingState = RecordingState.STOPPED;
+		this.player.removeLoopListerner(this);
+		if (this.recordingSample != null) {
+			this.recordedSample = this.recordingSample.toByteArray();
+			this.recordingSample = null;
+		}
+		if (this.recordedSample != null) {
+			this.loopStore.addSample(this.recordedSample);
+			this.recordedSample = null;
+			this.recordingSample = null;
 		}
 	}
-	
+
+	@Override
 	public void loopStarted() {
-		if(recordingState == RecordingState.WAITING){
-			recordingState = RecordingState.RECORDING;
-			recordedSample = null;
-			recordingSample = new ByteBuffer();
-			Thread recordThread = new Thread(){
+		if (this.recordingState == RecordingState.WAITING) {
+			this.recordingState = RecordingState.RECORDING;
+			this.recordedSample = null;
+			this.recordingSample = new ByteArrayOutputStream();
+			Thread recordThread = new Thread() {
+				@Override
 				public void run() {
 					try {
-						DataLine.Info info = new DataLine.Info(TargetDataLine.class, appConfig.getAudioFormat());
-//						checkState(AudioSystem.isLineSupported(info), "Line not supported");
+						DataLine.Info info = new DataLine.Info(TargetDataLine.class,
+								Recorder.this.appConfig.getAudioFormat());
+						// checkState(AudioSystem.isLineSupported(info),
+						// "Line not supported");
 						TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
-						line.open(appConfig.getAudioFormat());
+						line.open();
 						line.start();
 						log.info("Start capturing...");
 						AudioInputStream ais = new AudioInputStream(line);
 						log.info("Start recording...");
-						while (recordingState == RecordingState.RECORDING) {
-							recordingSample.append(ais.read());
+						while (Recorder.this.recordingState == RecordingState.RECORDING) {
+							byte[] buffer = new byte[2];
+							int bytesRead = ais.read(buffer);
+							Recorder.this.recordingSample.write(buffer, 0, bytesRead);
 						}
+						log.info("Stop recording...");
 
 					} catch (LineUnavailableException ex) {
 						ex.printStackTrace();
@@ -79,11 +89,11 @@ public class Recorder implements LoopListener{
 				}
 			};
 			recordThread.start();
-		}else if(recordingState == RecordingState.RECORDING){
-			recordedSample = recordingSample.toArray();
-			recordingSample = new ByteBuffer();
-		}else{
-			player.removeLoopListerner(this);
+		} else if (this.recordingState == RecordingState.RECORDING) {
+			this.recordedSample = this.recordingSample.toByteArray();
+			this.recordingSample = new ByteArrayOutputStream();
+		} else {
+			this.player.removeLoopListerner(this);
 		}
 	}
 }
